73412d900d502e6efd151bd41d2d3cbcd8aad14c
sserges <agounkeserge@gmail.com>
2021-12-27 11:17:30 -0500

    Arrays & Tuples

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,18 @@ let company: string = 'Trascompany'
 let isPublished: boolean = true
 let x: any = 'Hello'
 
+let ids: number[] = [1, 2, 3, 4]
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true]
+
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+  [1, 'Brad'],
+  [2, 'John'],
+  [3, 'Jill'],
+]
+
 console.log('ID: ', id)
