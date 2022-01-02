f2bcc91f08891b4594dce5d34a8972e19bd1b0e2
sserges <agounkeserge@gmail.com>
2022-01-02 13:21:06 -0500

    Function Interface

--- src/index.ts
+++ src/index.ts
@@ -79,5 +79,15 @@ const user1: UserInterface = {
   name: 'John',
 }
 
+interface MathFunc {
+  (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
+
+console.log('add', add(5, 4))
+console.log('sub', sub(4, 2))
+
 type Point = number | string
 const p1: Point = 1
