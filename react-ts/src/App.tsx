652ba30e562ab5406ba07c2f35afe605b7e32d18
sserges <agounkeserge@gmail.com>
2022-01-03 11:13:28 -0500

    TypeScript With React

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='Hello World' color='red' />
+    </div>
+  );
+}
+
+export default App;
